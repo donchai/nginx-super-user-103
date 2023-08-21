8affdd9326991264d1eb92330583a7f3f202619b
donchai <donchai@gmail.com>
2023-08-21 21:31:34 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NGINX Super User 2.0'
+copyright = '2023, Don Chai'
+author = 'Don Chai'
 
 release = '0.1'
 version = '0.1.0'
